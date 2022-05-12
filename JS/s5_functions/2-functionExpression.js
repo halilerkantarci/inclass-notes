6ae6b14109dd5a9c354ebb0d8a70e05fb35c9f72
halil <halilerk77@icloud.com>
2022-05-12 21:54:49 +0300

    q2

--- JS/s5_functions/2-functionExpression.js
+++ JS/s5_functions/2-functionExpression.js
@@ -1,7 +1,21 @@
 //! BU YÖNTEM ÇOK KULLANILMAZ
 
-const TekMi = function (x) {
-  return x % 2 === 0 ? "çift" : "tek";
+//******* ÖRNEK 1 */
+// const TekMi = function (x) {
+//   return x % 2 === 0 ? "çift" : "tek";
+// };
+// const sayi = Number(prompt("sayi gir"));
+// console.log(`${sayi} ${TekMi(sayi)}`);
+
+//******* ÖRNEK 2: find the bigest number */
+const buyukBul = function (a, b, c) {
+  if (a > b && a > c) {
+    return `${a} is the biggest.`;
+  } else if (b > a && b > c) {
+    return `${b} is the biggest`;
+  } else return `${c} is the biggest`;
 };
-const sayi = Number(prompt("sayi gir"));
-console.log(TekMi(3));
+const sayi1 = Number(prompt("sayi gir"));
+const sayi2 = Number(prompt("sayi gir"));
+const sayi3 = Number(prompt("sayi gir"));
+console.log(buyukBul(sayi1, sayi2, sayi3));
