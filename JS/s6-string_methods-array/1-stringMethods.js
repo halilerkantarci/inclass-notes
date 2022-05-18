67b4e41ffd6a0156965f8582ceae84e11ca97da0
halil <halilerk77@icloud.com>
2022-05-18 17:01:18 +0300

    charAt

--- JS/s6-string_methods-array/1-stringMethods.js
+++ JS/s6-string_methods-array/1-stringMethods.js
@@ -1,8 +1,34 @@
 //* --------------------------------
 //* concat() - immutable
 //* --------------------------------
+
 const str1 = "Clarusway";
-const str2 = "Hello";
-const srt3 = "World";
+const str2 = " Hello";
+const str3 = " World";
+//! Klasik yöntem ile string tanımlanırsa bu string bir primitive değişkendir
 const str4 = str1 + str2;
 console.log(str4);
+
+console.log(str4.concat(str3));
+console.log(str4.concat(" Turkey", " poland"));
+const str6 = " selam";
+const str7 = " good day";
+const str8 = str4.concat(str6).concat(str7);
+console.log(str8);
+//! NON-PRİMİTEVE STRİNG TANIMLAMA
+// const str5 = new String("non-primitive string");
+// console.log(str5, typeof str5);
+
+//* Normalde primitive veri tiplerinin property veya metodu olmaz.
+//* Ancak,Javascript otomatik olarak primitive String'ler için object wrapper oluşturur.
+//* Bu sayede, String nesnelerine ait olan fonksiyonlar veya özellikleri
+//* primitive string ile kullanılabilir hale gelir.
+//* Sonrasında oluşan bu object bellekten otomatik olarak kaldırılır.
+
+//* --------------------------------
+//* charAt() - immutable
+//* --------------------------------
+console.log(str8.charAt()); //! 0. indekste bulunan charı getirir
+console.log(str8.charAt(4));
+console.log(str8.charAt(str8.length - 1));
+// console.log(str8.indexOf("a"));
