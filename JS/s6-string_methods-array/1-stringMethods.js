c6f3e95606b99ef51c9548a6c61c920cbbb67819
halil <halilerk77@icloud.com>
2022-05-18 14:24:25 +0300

    stringMethods

---
+++ JS/s6-string_methods-array/1-stringMethods.js
@@ -0,0 +1,8 @@
+//* --------------------------------
+//* concat() - immutable
+//* --------------------------------
+const str1 = "Clarusway";
+const str2 = "Hello";
+const srt3 = "World";
+const str4 = str1 + str2;
+console.log(str4);
