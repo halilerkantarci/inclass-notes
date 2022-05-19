61b6478d0518a564b743f49436f1c5f817e32c5f
halil <halilerk77@icloud.com>
2022-05-19 14:22:32 +0300

    arrays

--- JS/s6-string_methods-array/2-arrays.js
+++ JS/s6-string_methods-array/2-arrays.js
@@ -4,3 +4,18 @@
 
 //* DİZİ TANIMLAMA
 //* ------------------------------------------------
+
+//! 1:YÖNTEM (ARRAY LİTERAL) - BU YÖNTEMİ KULLANICAZ
+const isimler = ["ahmet", "mehmet", "ismet", "saffet"];
+console.log(isimler);
+console.log(typeof isimler[0], isimler[0]);
+
+//! 2:YÖNTEM (OBJECT CONSTRUCTOR)
+const diller = new Array("Phyton", "Java", "C++", "JS", "Go");
+console.log(diller);
+
+const number = new Array(3, 2, 1);
+console.log(number);
+
+const sayilar = new Array(10);
+console.log(sayilar); //! 10 elemanlık boş bir array oluşturur
