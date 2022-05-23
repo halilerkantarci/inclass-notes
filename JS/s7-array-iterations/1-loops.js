9371fdb88555d5d29b1f07ff6e04f56e5348e258
halil <halilerk77@icloud.com>
2022-05-23 22:16:11 +0300

    foreach

--- JS/s7-array-iterations/1-loops.js
+++ JS/s7-array-iterations/1-loops.js
@@ -62,16 +62,45 @@ console.log(ogrenciBul(ogrIsim));
 const ogrenciBul1 = function (isim) {
   let sayac = 0;
   for (let ogrenci of ogrenciler) {
-    if (ogrenci == isim) {
-      sayac++;
-    }
+    ogrenci === isim ? sayac++ : null; //! ternary
+    // ogrenci === isim && sayac++; //! short-circuit yöntemi: && --> koşul doğru ise ifadeyi çağırır
+    // ogrenci === isim || sayac--; //! short-circuit yöntemi: || --> koşul yanlış ise ifadeyi çağırır
   }
-  if (sayac == 0) {
-    return `${isim},${sayac} adet bulundu`;
-  } else {
-    return `${isim} ismiden ${sayac} tane var`;
-  }
+  return !sayac
+    ? `${isim},${sayac} adet bulundu`
+    : `${isim} ismiden ${sayac} tane var`;
 };
 
 let ogrIsim1 = "AHMet".toLocaleLowerCase();
 console.log(ogrenciBul1(ogrIsim1));
+
+//* ======================================================
+//*                   FOREACH LOOP
+//* ======================================================
+
+const fiyatlar = [100, 250, 50, 59];
+// let x = fiyatlar.forEach(yazdir);
+// function yazdir(value) {
+//     console.log(value);
+// }
+
+fiyatlar.forEach((value) => console.log(value));
+
+//* FİYATLAR DİZİSİNDEKİ ELEMANLARIN TOPLAMINI YAZDIR
+let toplam = 0;
+fiyatlar.forEach((fiyat) => (toplam += fiyat));
+console.log(toplam);
+
+//! NOT : foreach metodu void bir metottur.Yani herhangi bir değer döndürmez
+
+// --------------------------  ÖRNEK ----------------------
+let toplam1 = 0;
+fiyatlar.forEach((value, indis, dizi) => {
+  toplam1 += value;
+  console.log(`${indis}.iterasyon toplamı:${toplam1}`);
+  dizi[indis] = value + 10;
+});
+console.log(fiyatlar);
+
+//*-------------- ÖDEV -------------------
+//* fiyatlar dizisinde her bir fiyata %10 zam yapalım.
