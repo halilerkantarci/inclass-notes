ae1bd51510aff538f7549af5b385693199fa24ab
halil <halilerk77@icloud.com>
2022-06-02 23:02:39 +0300

    completed

--- JS/s11-js-checkout/checkout.js
+++ JS/s11-js-checkout/checkout.js
@@ -1,30 +1,25 @@
-const taxRate = 0.18;
-const shippingPrice = 15.0;
+// const taxRate = 0.18;
+// const shippingPrice = 15.0;
 
 window.addEventListener("load", () => {
-  // set item to LocalStorage
-  // biz silene kadar durur
-  localStorage.setItem("taxRate", taxRate);
-  localStorage.setItem("shippingPrice", shippingPrice);
+  calculateCartTotal();
+  //set item to LocalStorage
+  // localStorage.setItem("taxRate", taxRate);
+  // localStorage.setItem("shippingPrice", shippingPrice);
 
-  // set item to SessionStorage
-  // oturumu kapattığınızda bu değerler kaybolur
-  //!   sessionStorage.setItem("taxRate", taxRate);
-  //!   sessionStorage.setItem("shippingPrice", shippingPrice);
+  //set item to SessionStorage
+  //  sessionStorage.setItem("taxRate", taxRate);
+  //  sessionStorage.setItem("shippingPrice", shippingPrice);
 });
 
-// capturing method
+//capturing method
 let productsDiv = document.querySelector(".products");
 productsDiv.addEventListener("click", (event) => {
-  // if (e.target.classList.contains("minus"))
-  //alttakiyle üstteki aynı şey
-  //! clas name dersek; o elementin tüm classları yazılmalı ("minus pls btn")
   if (event.target.className == "minus") {
     let quantityP = event.target.nextElementSibling;
     if (quantityP.innerText > 1) {
       quantityP.innerText--;
-      // dedeye çıkmış olduk
-      // parameter == productInfoDiv
+      //parameter == selected productInfoDiv
       calculateProductAndCartTotal(event.target.parentElement.parentElement);
     } else {
       if (confirm("Product will be deleted?")) {
@@ -32,35 +27,64 @@ productsDiv.addEventListener("click", (event) => {
         calculateCartTotal();
       }
     }
-
+    // console.log(typeof event.target.nextElementSibling.innerText);
     // console.log("minus button clicked");
-
-    //? classlist dersem o elementin tek bir classını yazmam yeterli
   } else if (event.target.classList.contains("plus")) {
     event.target.previousElementSibling.innerText++;
-    // parameter == productInfoDiv
+    //parameter == selected productInfoDiv
     calculateProductAndCartTotal(event.target.parentElement.parentElement);
-
     // console.log("plus button clicked");
   } else if (event.target.classList.contains("remove-product")) {
-    event.target.parentElement.parentElement.parentElement.remove();
-    calculateCartTotal();
+    if (confirm("Product will be deleted?")) {
+      event.target.parentElement.parentElement.parentElement.remove();
+      calculateCartTotal();
+    }
 
     // console.log("remove button clicked");
   } else {
-    // console.log("other clicks");
+    // console.log("other element clicked");
   }
-  // tıklanan elementi gösterir
-  //   console.log(event.target);
 });
 
-//? ==================================
-//! CALCULATE CART AND PRODUCT TOTALS
-//* ==================================
+//calculate cart and product totals
 const calculateProductAndCartTotal = (productInfoDiv) => {
-  let quantity = 
-  let price = 
+  //product calculation
+  // console.log(productInfoDiv);
+  let price = productInfoDiv.querySelector("strong").innerText;
+  let quantity = productInfoDiv.querySelector("#product-quantity").innerText;
+  let productTotalDiv = productInfoDiv.querySelector(".product-line-price");
+  productTotalDiv.innerText = (price * quantity).toFixed(2);
+  //cart calculation
   calculateCartTotal();
 };
 
-const calculateCartTotal = () => {};
+//calculate cart totals
+const calculateCartTotal = () => {
+  //nodeList Div
+  let productsTotalPriceDivs = document.querySelectorAll(".product-line-price");
+  // console.log(productsTotalPriceDivs);
+  let subtotal = 0;
+  productsTotalPriceDivs.forEach((eachProductTotalDiv) => {
+    subtotal += parseFloat(eachProductTotalDiv.innerText);
+  });
+  console.log(subtotal);
+  let taxPrice = subtotal * localStorage.getItem("taxRate");
+  console.log(taxPrice);
+
+  let shippingPrice =
+    subtotal > 0 ? parseFloat(localStorage.getItem("shippingPrice")) : 0;
+
+  let cartTotal = subtotal + taxPrice + shippingPrice;
+
+  document.querySelector("#cart-subtotal p:nth-child(2)").innerText =
+    subtotal.toFixed(2);
+
+  document.querySelector("#cart-tax p:nth-child(2)").innerText =
+    taxPrice.toFixed(2);
+
+  document.querySelector("#cart-shipping p:nth-child(2)").innerText =
+    shippingPrice.toFixed(2);
+
+  document.querySelector("#cart-total").lastElementChild.innerText =
+    cartTotal.toFixed(2);
+};
