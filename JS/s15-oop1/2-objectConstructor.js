f56a4e049519f24a6ca8a63f792181ec30716127
halil <halilerk77@icloud.com>
2022-06-18 12:30:08 +0300

    .

--- JS/s15-oop1/2-objectConstructor.js
+++ JS/s15-oop1/2-objectConstructor.js
@@ -10,14 +10,22 @@
 
 //? OBJECT CONSTRUCTOR
 // "B" büyük yazılarak,object olduğu ima edildi
-function Book(title1, author1, year1) {
-  this.title = title1;
-  this.author = author1;
-  this.year = year1;
+
+//! *********** ilk .js de her bir kitap için ayrı ayrı obje oluşturmam lazımdı. burada tek bir function yapıp obje halinde kullanarak bellekten yer kazanıyoruz //!**
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
   this.getSummary = function () {
-    return `${this.title} was written by ${this.author} in ${this.year}`;
+    return `${this.title} was written by ${this.author} in ${this.year} `;
   };
 }
+
+//! book fonksiyonuna özellik ekleme
+
+Book.prototype.getAge = function () {
+  return new Date().getFullYear() - this.year;
+};
 
 //? new keyword'u Book Constructor'ini parametereler ile cagirmaktadir.
 //? Constructor ise Book nesnesinden bir ornek (instance) olusturmaktadir.
@@ -29,5 +37,38 @@ const book1 = new Book("kasagi", "ömer seyfettin", 1920);
 const book2 = new Book("sinekli bakkal", "halide edip adıvar", 1956);
 
 console.log(book1);
-console.log(book2);
+console.log(typeof book2, book2);
 console.log(book2.getSummary());
+
+// ADDING A PROPERTY
+book1.price = 100;
+//! book2 ye eklemez
+console.log(book1, book2);
+
+Book.prototype.type = "novel";
+
+console.log(book1);
+console.log(book1.getAge());
+console.log(book2.getAge());
+console.log(Book);
+//! prototype görme
+console.log(Book.prototype);
+console.log(book2.type);
+
+//! Prototype, belirli bir Nesne'nin (Object) tum instance'larina kolay bir
+//! sekilde metotlar tanimlamaya izin vermektedir.
+//! Prototip alaninda bir metot tanimlamanin guzelligi bu metot'un olusan
+//! tum instance'lerin belleginde yer kaplamamasi ancak tum instance'larin bu metota
+//! ulasabilmesidir.
+
+//* Ornegin Book nesnesinin tum instance'lari getAge() fonksiyonunu miras alabilir.
+//* Ancak, getAge() fonksiyonu bellekte sadece tek bir yer kaplamaktadir.
+
+//* Bir nesnenin prototiplerine .prototype ile erisilebilir.
+//* Ancak bir instance'in prototiplerine .__proto__ ile erisilmektedir.
+
+//!Object'lerin proto alanini gormk icin .protoype
+console.log(Book.prototype);
+
+//!instance'larin proto alanini gormk icin .__proto__
+console.log(book1.__proto__);
